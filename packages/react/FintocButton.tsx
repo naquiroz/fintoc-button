9917ed1496d3dc326901fdb9d7026ee05d16b4f6
Nicolas <naquiroz@uc.cl>
2022-01-06 00:10:44 -0300

    Complete seemingly functional react button
    Co-authored-by: Daniel Leal <daleal@users.noreply.github.com>

--- packages/react/FintocButton.tsx
+++ packages/react/FintocButton.tsx
@@ -1,23 +1,28 @@
-import { getFintocObject } from "~/core";
+import { useEffect, useState } from 'react';
+import { getFintocObject } from '~/core';
 
 export async function FintocButton(props: {}) {
-  const Fintoc = await getFintocObject(); // ESTO VA ACÁ O EN EL ON MOUNT????
-  const [widget, setWidget] = useState(null);
+  const [widget, setWidget] = useState<FintocWidget|null>(null);
 
   const onClick = () => {
     if (!widget) {
-      console.log('F');
+      throw Error('Error mounting the Fintoc widget');
     } else {
       widget.open();
     }
   };
 
-  onMount(() => {
-    setWidget(Fintoc.create(props));
-  });
+  useEffect(() => {
+    async function createWidget() {
+      const Fintoc = await getFintocObject();
+      setWidget(Fintoc.create(props));
+    }
+
+    createWidget();
+  }, []);
 
   return (
-    <button click={onClick}>
+    <button onClick={onClick}>
       Usa Fintoc!
     </button>
   )
